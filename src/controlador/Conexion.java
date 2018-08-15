525eca0dbe70a285417ad4bacfde11de87919caf
Franz Flores <andresfloresgallardo@hotmail.com>
2018-08-15 14:43:42 -0500

    Creacion Persistencia
    Conexion y Persistencia creadas

---
+++ src/controlador/Conexion.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author franzandresflores
+ */
+public class Conexion {
+
+    private static EntityManager manager;
+    private static final String NAME_EMPU = "InmobiliariaPU";
+
+    public static EntityManagerFactory sesion() {
+        return Persistence.createEntityManagerFactory(NAME_EMPU);
+    }
+
+    public static EntityManager getManager() {
+        if (manager == null) {
+            manager = sesion().createEntityManager();
+        }
+        return manager;
+    }
+
+}
